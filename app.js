f528ce9e4c3bcdd9176e55f9749c5910c53042dc
Benjamin Soung <soungbenjamin@gmail.com>
2017-07-24 13:13:39 -0400

    working on creating a cookie

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("Express");
 const app = express();
 const hbs = require("express-handlebars");
+const cookieParser = require("cookie-parser");
 const port = 3000;
+
+// cookies
+app.use(cookieParser());
 
 //set up handlebars
 app.engine("handlebars", hbs({ defaultLayout: "main" }));
@@ -11,6 +15,13 @@ app.set("view engine", "handlebars");
 app.get("/", (req, res) => {
   res.render("index", {});
 });
+
+app.post("/", (req, res) => {
+	// const id = req.params.id;
+  const favorites = req.cookies.favorites || [];
+  res.cookie("favorites", favorites.push(id));
+  res.redirect("/");
+})
 
 app.listen(port, (err, data) => {
   console.log(`server listening on localhost:${port}`);
