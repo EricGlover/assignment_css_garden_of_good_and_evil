0b4e74fc192ca8cfd7a20111fd2ef6424fd7ecd5
Eric Glover <etgt28@mail.missouri.edu>
2017-07-24 17:31:56 -0500

    completed bio, working on resume

--- app.js
+++ app.js
@@ -41,15 +41,15 @@ app.get("/", (req, res) => {
     //res.render("index", { cookieObj });
     const view = viewData(cookieObj);
 
-    console.log(view, 'what is this')
+    console.log(view, "what is this");
 
-    res.render('index', {
-    	cookieObj, 
-    	dislikes: view.dislikes, 
-    	likes: view.likes,
-    	bio: view.bio
-    })
-
+    res.render("index", {
+      cookieObj,
+      dislikes: view.dislikes,
+      likes: view.likes,
+      bio: view.bio,
+      resume: view.resume
+    });
   } else {
     res.render("index");
   }
@@ -57,32 +57,57 @@ app.get("/", (req, res) => {
 
 //cookie information -> sensible data for views
 function viewData(cookieObj) {
-	let favoriteColor = cookieObj['favorite-color'];
-  let favoriteFood = cookieObj['favorite-food'];
-  let insanity = cookieObj['insanity'];
+  let favoriteColor = cookieObj["favorite-color"];
+  let favoriteFood = cookieObj["favorite-food"];
+  let insanity = cookieObj["insanity"];
 
-  let bio = '';
-  let location = '';
+  let bio = "";
+  let location = "";
+  let dreams = "";
+  let memories = "";
+  let education = "";
+  let resume = {
+    jobs: []
+  };
 
   let viewData = {
     dislikes: [],
     likes: [favoriteColor, favoriteFood, insanity],
-    bio: bio
+    bio: bio,
+    resume: resume
   };
 
-  console.log(cookieObj, 'what is this')
+  console.log(cookieObj, "what is this");
 
   if (cookieObj.morality == "evil") {
-  	location = 'Canada';
+    location = "Canada";
+    hobbies = "Strollin on the beach";
     //handle dislikes
-    viewData.dislikes = ['kittens', 'coffee', 'hippies'];
-    console.log(viewData.dislikes)
+    viewData.dislikes = ["kittens", "coffee", "hippies"];
+    memories = "I remember the good times, killing ants as a child.";
+    resume.jobs = ["Kidnapper Extraordinaire"];
+    console.log(viewData.dislikes);
   } else if (cookieObj.morality == "good") {
-  	location = 'USA';
-    viewData.dislikes = ['mice????', 'rainy days', 'hippies']
+    location = "USA";
+    hobbies =
+      "Volunteering my time to local charities, re-reading biographies about Ghandi and never throwing kid's icecream cones on the ground. ";
+    viewData.dislikes = ["mice????", "rainy days", "hippies"];
+    memories = "I remember the good times, watching basebal games.";
+    resume.jobs = ["Second grade teacher"];
+  }
+  if (insanity > 70) {
+    hobbies =
+      "Driving on the beach - sometimes I bring my couch - , wrestling sharks while scuba diving; and doing bit manipulation in C.";
+    education = "I had a very educational year once.";
+    memories = "I REMEMBER THE GOOD TIMES!!!!!!!";
+    //
+    resume.jobs.push("Cleaning my cat bootsie");
+    resume.jobs.push("Pope");
+  } else if (insanity <= 70) {
+    education = "I went to Oxford...no big deal.";
   }
 
-  viewData.bio = `I grew up in ${location} and my favorite color is ${favoriteColor}.`
+  viewData.bio = `I grew up in ${location} and my favorite color is ${favoriteColor}. My hobbies include ${hobbies}. My fondest childhood memory ${memories}. ${education}`;
   return viewData;
 }
 
